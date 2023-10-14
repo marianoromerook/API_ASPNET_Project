2a0924ce4eaa567790f7d72a373a70baf4b5cbc9
Mariano Romero <marianoromero40@gmail.com>
2023-10-14 19:05:22 -0300

    Mejoras en la sección de descuentos

---
+++ Models/StoreDiscountConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace API_RESTful_Project.Models
+{
+    public class StoreDiscountConfiguration
+    {
+        public int StoreId { get; set; }
+        public Dictionary<string, decimal>? DiscountMapping { get; set; }
+        public decimal GeneralDiscount { get; set; }
+
+    }
+}
---
+++ Services/IStoreDiscountService.cs
@@ -0,0 +1,9 @@
+﻿using API_RESTful_Project.Models;
+
+namespace API_RESTful_Project.Services
+{
+    public interface IStoreDiscountService
+    {
+        StoreDiscountConfiguration GetStoreDiscountConfiguration(int storeId);
+    }
+}
